c10860a7532ea4786180f584cd0d2aee46f65379
Felipe <felipeparada99@hotmail.com>
2022-06-25 18:08:13 -0300

    commit agregando funcionalidades del carrito

--- scripts/carrito.js
+++ scripts/carrito.js
@@ -1,18 +1,51 @@
-carrito = []
-
 function mostrarCarro(){
+    let contador = 0
+    let carrito = localStorage.getItem("carrito")
     let contenedor = document.getElementById("mainContainer")
-    contenedor.innerHTML = `<div class="centradoLinea"><h1 class="subtitulo ">Productos agregados al carrito</h1></div>`
-
-    if(carrito.lengh != 0){
+    if(carrito != null){
+        contenedor.innerHTML = `<div class="centradoLinea"><h1 class="subtitulo ">Productos agregados al carrito</h1></div>`
+        carrito = JSON.parse(carrito)
         carrito.forEach(it => {
             let itHTML=`<div class="itemCarrito">
-                            <h5>${it.nombre}</h5>
-                            <span>-</span>
-                            <h6>${it.precio}</h6>
+                            <div class="innerContainer">
+                                <div class="rightContainer">
+                                    <img src="${it.img}" alt="">
+                                    <div>
+                                        <h5>${it.nombre}</h5>
+                                        <h6>${it.precio}</h6>
+                                    </div>
+                                </div>
+                                <div class="leftContainer">
+                                    <a id="${contador}" href="#" class="btn btn-primary">Eliminar</a>
+                                </div>
+                            </div>
                         </div>`
             contenedor.innerHTML += itHTML
+            contador++
         });
+    }      
+}
+mostrarCarro()
+
+
+function agregarListeners(){
+    let botones = document.querySelectorAll(".btn, .btn-primary")
+    if(botones.length != 0){
+        botones.forEach(element => {
+            element.addEventListener('click',()=>{
+                eliminarItem(element.id)
+                element.className = "btn .btn-primary"
+            })
+        }); 
     }
-    
+}
+
+agregarListeners()
+
+function eliminarItem(id){
+    debugger
+    let carrito = JSON.parse(localStorage.getItem("carrito"))
+    carrito = carrito.filter(producto => producto.id != id)
+    localStorage.setItem("carrito",JSON.stringify(carrito))
+    mostrarCarro()
 }
--- scripts/scriptTienda.js
+++ scripts/scriptTienda.js
@@ -5,8 +5,21 @@ botones.forEach(boton => {
         let Nombre = elem.querySelector("h5").innerText
         let Precio = elem.querySelector("h6").innerText
         let img = elem.querySelector("img").src
-        carrito.push(new item(Nombre,"bicicleta",Precio,img))
-        console.table(carrito)
-        mostrarCarro()
+        
+        boton.className = "btn .btn-primary"
+
+
+
+        let carrito = localStorage.getItem("carrito")
+        if(carrito == null){
+            carrito2 = []
+            carrito2.push(new item(Nombre,"bicicleta",Precio,img))
+            localStorage.setItem("carrito",JSON.stringify(carrito2))
+        }else{
+            carrito = JSON.parse(carrito)
+            carrito.push(new item(Nombre,"bicicleta",Precio,img))
+            localStorage.setItem("carrito",JSON.stringify(carrito))
+        }
+        
     })
 });
